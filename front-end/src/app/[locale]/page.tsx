9956ea710b0741f3b3e5f4a38d2229ac654c7d8d
PharaEthan <ethan1.hernou@epitech.eu>
2025-09-09 13:02:12 +0200

    feat(metadata): add metadata to all website pages

--- front-end/src/app/[locale]/page.tsx
+++ front-end/src/app/[locale]/page.tsx
@@ -1,19 +1,33 @@
-import { Locale            } from "next-intl";
-import { setRequestLocale  } from "next-intl/server";
-import { use               } from "react";
-import { OMGGTestimonials  } from "@/components/OMGG/Section/Testimonials";
-import { OMGGBlog          } from "@/components/OMGG/Section/Blog";
-import { OMGGOffers        } from "@/components/OMGG/Section/Offers";
-import { OMGGNewsLetter    } from "@/components/OMGG/Section/NewsLetter";
-import { OMGGHero          } from "@/components/OMGG/Section/Hero";
-import { OMGGAbout         } from "@/components/OMGG/Section/About";
-import { OMGGLogos         } from "@/components/OMGG/Section/Logos";
+import { Locale                            } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import { OMGGTestimonials                  } from "@/components/OMGG/Section/Testimonials";
+import { OMGGBlog                          } from "@/components/OMGG/Section/Blog";
+import { OMGGOffers                        } from "@/components/OMGG/Section/Offers";
+import { OMGGNewsLetter                    } from "@/components/OMGG/Section/NewsLetter";
+import { OMGGHero                          } from "@/components/OMGG/Section/Hero";
+import { OMGGAbout                         } from "@/components/OMGG/Section/About";
+import { OMGGLogos                         } from "@/components/OMGG/Section/Logos";
+import type { Metadata                     } from "next";
 
 import FadeInWhenVisible from "@/components/Animator/Fade/FadeInWhenVisible";
 
-export default function Home({ params }: { params: Promise<{locale: Locale}> })
+export async function generateMetadata({ params }: { params: Promise<{ locale: Locale }> }): Promise<Metadata> {
+  const { locale } = await params;
+  const t          = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+    openGraph: {
+      title: t("title"),
+      description: t("description"),
+    },
+  };
+}
+
+export default async function Home({ params }: { params: Promise<{ locale: Locale }> })
 {
-  const { locale } = use(params);
+  const { locale } = await params;
 
   setRequestLocale(locale);
 
