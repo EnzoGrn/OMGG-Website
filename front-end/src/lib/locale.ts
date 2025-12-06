6a25cf76fc487e92aa530d8c5e7ee895cdb410b3
PharaEthan <ethan1.hernou@epitech.eu>
2025-12-06 23:09:28 +0100

    feat(site-policy): add legals documents
    Fetch strapi to get legal document with slug

--- front-end/src/lib/locale.ts
+++ front-end/src/lib/locale.ts
@@ -1,8 +1,7 @@
 import { cookies } from "next/headers"
 
-export async function getLocale(): Promise<string>
-{
+export async function getLocale(): Promise<string> {
   const cookieStore = await cookies();
   const locale = cookieStore.get('locale');
-  return locale?.value ?? 'en'
+  return locale?.value ?? 'fr'
 }
